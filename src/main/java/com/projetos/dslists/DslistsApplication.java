789ab15838e3952b4a9245a38b4e90e1a46cd883
norwalneto <norwalgomess@gmail.com>
2024-11-11 10:21:35 -0300

    Projeto do intensivão Java com Springboot.

---
+++ src/main/java/com/projetos/dslists/DslistsApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.dslists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetos/dslists/DslistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.dslists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
